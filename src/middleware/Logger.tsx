acf4ad7be380390e74ebd2a59c14adf92c186869
SShipon <nxshipon@gmail.com>
2024-06-27 00:49:07 +0100

    crate a Logger add middelware

--- src/middleware/Logger.tsx
+++ src/middleware/Logger.tsx
@@ -1,7 +1,8 @@
 const Logger =  (state) => (next) =>(action) =>{
-    console.log(state)
-    console.log(next)
-    console.log(action)
+    console.log("Current State ==>", state.getState())
+    console.log("Action ==>", action);
+    next(action)
+    console.log("Current State ==>", state.getState())
 }
 
 export default Logger
